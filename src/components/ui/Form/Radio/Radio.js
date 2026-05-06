788bafd863dbcce4410e30ef55be352450b3a3aa
RzayevTaleh01 <taleh.rzayev.i@asoiu.edu.az>
2026-05-06 19:56:40 +0400

    upx

---
+++ src/components/ui/Form/Radio/Radio.js
@@ -0,0 +1,24 @@
+import styles from "@/components/ui/Form/Form.module.css";
+
+export default function SgRadio(props) {
+    const {label, name, id, required, className, readonly, data_key, data_variant, size, data_index, disabled, value, checked, loading, isInvalid, onChange = () => {}} = props
+
+    const handleChange = (e) => {
+        if (disabled || readonly || loading) {
+            e.preventDefault()
+            return
+        }
+        (onChange)?.(e)
+    }
+
+    return (
+        <>
+            <div className={[styles['input-container']].join(' ').trim()}>
+                <div className={[styles["input-wrapper"], className, styles["input-wrapper--checkbox"], isInvalid && styles['input-wrapper--error']].join(' ').trim()}>
+                    <input className={styles["radio"]} type="radio" data-key={data_key} data-index={data_index} name={name} id={id} onChange={handleChange} disabled={disabled} value={value} readOnly={readonly} required={required} checked={checked} data-variant={data_variant} />
+                    <label className={[styles["label"], styles["label--checkbox"]].join(' ').trim()} htmlFor={id}>{label}</label>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ src/components/ui/Form/index.js
@@ -0,0 +1,17 @@
+import SgCheckbox from "@/components/ui/Form/Checkbox";
+import SgCheckboxGroup from "@/components/ui/Form/CheckboxGroup";
+import SgFormGroup from "@/components/ui/Form/FormGroup";
+import SgRadio from "@/components/ui/Form/Radio";
+import SgSwitch from "@/components/ui/Form/Switch";
+import SgInput from "@/components/ui/Form/Input";
+import SgFile from "@/components/ui/Form/File";
+
+export {
+    SgCheckbox,
+    SgCheckboxGroup,
+    SgFormGroup,
+    SgRadio,
+    SgSwitch,
+    SgInput,
+    SgFile
+}
